97fa7e5821b71437db26054a646a8035f276dced
yhoutzager <38581420+yhoutzager@users.noreply.github.com>
2021-11-30 19:03:52 +0100

    Add reader helper

---
+++ src/day01.py
@@ -0,0 +1,5 @@
+from filereader import *
+
+if __name__ == '__main__':
+	file_lines = read_lines("test")
+	file_lines = read_lines("day01")
---
+++ src/filereader.py
@@ -0,0 +1,17 @@
+def open_file(name):
+	return open("../input/" + name + ".txt", "r")
+
+def read_lines(name):
+	return open_file(name).read().splitlines()
+
+def read_split(name, delim):
+	return open_file(name).read().split(sep=delim)
+
+def read_comma_sep(name):
+	return read_split(name, ",")
+
+def trim(array):
+	return [x.strip() for x in array]
+
+def as_int(array):
+	return [int(x) for x in array]
