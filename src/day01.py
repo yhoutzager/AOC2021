648b9363ae8c6cf3fa19ef3df52c8c19b01bb687
yhoutzager <38581420+yhoutzager@users.noreply.github.com>
2021-12-01 06:16:10 +0100

    day 01

--- src/day01.py
+++ src/day01.py
@@ -1,5 +1,20 @@
 from filereader import *
 
 if __name__ == '__main__':
-	file_lines = read_lines("test")
-	file_lines = read_lines("day01")
+	# depths = as_int(read_lines("test"))
+	depths = as_int(read_lines("day01"))
+	counter = 0
+	for x in range(1, len(depths)):
+		if depths[x] > depths[x-1]:
+			counter += 1
+
+	print(counter)
+
+	counter = 0
+	for x in range(1, len(depths) - 2):
+		a = depths[x-1] + depths[x] + depths[x+1]
+		b = depths[x] + depths[x+1] + depths[x+2]
+		if b > a:
+			counter += 1
+
+	print(counter)
---
+++ src/day02.py
@@ -0,0 +1,5 @@
+from filereader import *
+
+if __name__ == '__main__':
+	file = as_int(read_lines("test"))
+	# file = as_int(read_lines("day02"))
