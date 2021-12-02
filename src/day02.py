2c8a5d8156e89001a8b72219c62e3322be275a66
yhoutzager <38581420+yhoutzager@users.noreply.github.com>
2021-12-02 06:19:09 +0100

    day 2

--- src/day02.py
+++ src/day02.py
@@ -1,5 +1,23 @@
 from filereader import *
 
 if __name__ == '__main__':
-	file = as_int(read_lines("test"))
-	# file = as_int(read_lines("day02"))
+	# file = read_lines("test")
+	file = read_lines("day02")
+	commands = [x.split(" ") for x in file]
+
+	pos_x, pos_y1, pos_y2, aim = 0, 0, 0, 0
+	for command in commands:
+		action = command[0]
+		mag = int(command[1])
+		if action == "forward":
+			pos_x += mag
+			pos_y2 += aim * mag
+		elif action == "down":
+			aim += mag
+			pos_y1 += mag
+		elif action == "up":
+			aim -= mag
+			pos_y1 -= mag
+
+	print(pos_x * pos_y1)
+	print(pos_x * pos_y2)
---
+++ src/day03.py
@@ -0,0 +1,5 @@
+from filereader import *
+
+if __name__ == '__main__':
+	file = read_lines("test")
+	# file = read_lines("day03")
