6888d27648c87c845b731b52736f0ac19f185e28
yhoutzager <38581420+yhoutzager@users.noreply.github.com>
2021-12-06 06:19:48 +0100

    day 06

--- src/day06.py
+++ src/day06.py
@@ -1,5 +1,22 @@
 from filereader import *
 
 if __name__ == '__main__':
-	file = read_lines("test")
-	file = read_lines("day06")
+	# file = as_int(read_comma_sep("test"))
+	file = as_int(read_comma_sep("day06"))
+
+	counter = [0]*9
+
+	for x in file:
+		counter[x] += 1
+
+
+	for x in range(256):
+		new_counter = [0]*9
+		for i, x in enumerate(counter):
+			if i == 0:
+				new_counter[8] += x
+				new_counter[6] += x
+			else:
+				new_counter[i-1] += x
+		counter = new_counter
+	print(sum(counter), counter)
